a57e49b3b917b9eebaae385532408b5800b88b25
RileyO170 <rjorourke17@gmail.com>
2024-06-15 20:10:01 -0700

    HW2_Test

---
+++ HW2 - Scrapy Production Scraper.py
@@ -0,0 +1,12 @@
+#   Assignment 2: Scrapy
+#   How to build a production scraper
+
+#   In the powershell below, setup a virtual environment, type:
+#   
+#   pip install virtualenv
+#   
+#   1.) This is done to avoid version conflicts down the road
+#   2.) It is best practice to create separate virtual environments for ea. Python project
+#   3.) Packages installed for a project are kept separate from other projects, so other's don't inadvertently break
+
+
